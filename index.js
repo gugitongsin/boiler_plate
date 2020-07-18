b301196b8d6c1a35871333f724cf664de9f6a3d2
jskee <gugitongsin@gmail.com>
2020-07-18 19:07:34 +0900

    0718

--- index.js
+++ index.js
@@ -2,15 +2,41 @@
 const express = require('express')
 const app = express();
 const port = 987;
-// mongoose
+
+//mongoose
+const config = require('./config/key')
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://kjs:kjs@boiler-plate.pieiv.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected....')).catch(err => console.log(err))
 
+//models
+const { User } = require('./models/user')
 
-var txt = 'Hello , my name is KJS';
+//body-Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}))   //application/x-www-form-urlencoded
+app.use(bodyParser.json());                         //application/json
+
+
+var txt = 'Hello , How are you';
+//get
 app.get('/', (req, res) => res.send(`${txt}`))
+
+//post
+app.post('/register', (req,res) => {
+    const user = new User(req.body);
+    user.save((err, doc) =>{
+        if(err) return res.json({success : false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+//listen
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
+
+
 
 
